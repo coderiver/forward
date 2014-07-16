acea192a1fe49cd254df3eca23fe9a04ef5394c3
Eugene Godun <eugene.godun@gmail.com>
2014-07-16 10:22:44 +0300

    registration

--- js/common.js
+++ js/common.js
@@ -1,14 +1,19 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
-	$(".js-select select").on("change",function(){
-		var	val = $(this).val();
-		$(this).parent().find(".js-select-text").text(val);
+	$(document).on("click", function(){
+		$(".js-select").removeClass("is-active");
 	});
 
+	$(".js-select select").on("change",function(event){
+		var	val = $(this).val();
+		$(this).parent().find(".js-select-text").text(val);
+		event.stopPropagation();
+	});
+
+	$(".js-select").on("click",function(event){
+		$(this).toggleClass("is-active");
+		event.stopPropagation();
+	});
 
 	function fotoramaInit() {
 	  	var $fotoramaDiv = $('.js-fotorama').on('fotorama:ready', function () {
